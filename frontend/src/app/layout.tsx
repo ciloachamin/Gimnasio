29ad14d514309a0e716ad4e0069234ff9a305f25
Israel <christho987@hotmail.com>
2023-11-21 16:35:25 -0500

    Create frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import { FC, PropsWithChildren } from "react";
+import FlowbiteContext from "./context/FlowbiteContext";
+import "./globals.css";
+import SessionAuthProvider from "./context/SessionAuthProvider";
+
+const RootLayout: FC<PropsWithChildren> = function ({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <FlowbiteContext>      <SessionAuthProvider> {children} </SessionAuthProvider> </FlowbiteContext>
+
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
